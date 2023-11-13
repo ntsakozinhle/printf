32342933431779d3f12af03118f79957e760ae50
KeaMohlala <keamogetsemhll@gmail.com>
2023-11-13 05:02:23 -0800

    task 0 compile with Wno format

--- char_str_percentage.c
+++ char_str_percentage.c
@@ -59,7 +59,7 @@ int _printf(const char *format, ...)
 	for (i = 0; format[i] != '\0'; i++)
 	{
 		if (format[i] != '%')
-			count += _putchar(format[i]);
+			count += putchar(format[i]);
 		else
 		{
 			i++;
