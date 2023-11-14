4e71dd13cd748682b5f502f1011871771caca65e
ntsakozinhle <ntsakosambo1@gmail.com>
2023-11-14 04:46:30 -0800

    task 2 and 3 changes

--- main.h
+++ main.h
@@ -12,6 +12,11 @@ int print_c(char c);
 int print_string(char *str);
 int format_specifier_conditions(char format_specifier, va_list ptr);
 int _printf(const char *format, ...);
+void binary_num(int num);
+int print_long_int(long int n);
+int hexa_int(long int n, char format_specifier);
+int octal_int(long int n);
+
 
 
 #endif
